33f6c364ac708abac01a8dfc89843513911f9ae9
R. Ilham Sastronegoro <7355835+radenpioneer@users.noreply.github.com>
2024-08-30 03:54:24 +0000

    RESET

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,4 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node'
 
 // https://astro.build/config
-export default defineConfig({
-    security: {
-        checkOrigin: true
-    },
-    output: 'server',
-    adapter: node()
-});
+export default defineConfig({});
--- src/data/db.ts
+++
@@ -1,5 +0,0 @@
-import { drizzle } from "drizzle-orm/bun-sqlite";
-import {Database} from "bun:sqlite"
-
-const sqliteDb = new Database(":memory:")
-export const db = drizzle(sqliteDb)
